26b1ab3ae4f4e900e01b9e2d6c71bd962cd4e272
Rida Habib <rhabib6244@gmail.com>
2021-03-05 13:59:29 -0500

    it should be fine

--- public/script.js
+++ public/script.js
@@ -2,28 +2,28 @@ async function windowActions() {
     console.log("Window loaded")
     const form = document.querySelector(".userform");
     const search = document.querySelector("#category");
-    const targetList = document.querySelector('.target-list');
+    const targetList = document.querySelector('.target-list')
 
-    const request = await fetch('/api');
-    const data = await request.json();
+    const request = await fetch('/api')
+    const data = await request.json()
+
+   
 
     form.addEventListener('submit', async (event) => {
         event.preventDefault();
         console.log("submit fired");
         const display = data.filter((record) => record.category.toUpperCase() === search.value.toUpperCase());
         while (targetList.firstChild) {
-          targetList.removeChild(targetList.firstChild)
-        };
+            targetList.removeChild(targetList.firstChild)
+          };
         display.forEach((item) => {
             const appendItem = document.createElement("li");
-            const name = item.name;
             appendItem.innerText = (item.name + "\n" + item.category + "\n" + item.address_line_1 + "\n" + item.city + ", " + item.state + "\n" + item.zip);
-            appendItem.className+="restaurant";
             targetList.append(appendItem);
         })
         console.log(display);
         console.table(display);
-    })
+    }) 
 
     search.addEventListener('input', (event) => {
         console.log('input', event.target.value);
